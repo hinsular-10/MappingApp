c602f933d9c4cfd6d5357afcc05be7a3713c070d
honeylene insular <insularhoneylene@gmail.com>
2025-06-28 01:57:06 -0600

    created park.cs, and models

---
+++ Models/Park.cs
@@ -0,0 +1,13 @@
+namespace ParksApi.Models
+{
+    public class Park
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+        public double Area { get; set; } // in square kilometers
+        public DateTime EstablishedDate { get; set; }
+        public string[] Activities { get; set; } // e.g., hiking, camping, bird watching
+    }
+}
